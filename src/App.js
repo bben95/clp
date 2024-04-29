ad2d5eccc12ad89086dce586dbf189e160f9f672
bben95 <benakabp@gmail.com>
2024-04-29 12:54:56 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Navbar } from './components/navbar';
 import { Sidenavbar } from './components/sidenavbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 
@@ -11,7 +12,11 @@ function App() {
       <Navbar/>
       <div className='flex'>
      <Sidenavbar/>
-     
+     {/* <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+   </Switch> */}
      </div>
 
     </div>
