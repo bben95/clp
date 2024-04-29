55968607487a52232e076e3a616c41b3d5fb04fe
bben95 <benakabp@gmail.com>
2024-04-29 18:23:39 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,133 +1,24 @@
 import './App.css';
-import NavBar1 from './sectionComponents/navBar/navBar1';
-import NavBar2 from './sectionComponents/navBar/navBar2';
-import NavBar3 from './sectionComponents/navBar/navBar3';
-import Hero1 from './sectionComponents/hero/hero1';
-import Hero2 from './sectionComponents/hero/hero2';
-import Hero3 from './sectionComponents/hero/hero3';
-import Hero4 from './sectionComponents/hero/hero4';
-import Hero5 from './sectionComponents/hero/hero5';
-import Hero6 from './sectionComponents/hero/hero6';
-import Hero7 from './sectionComponents/hero/hero7';
-import Hero8 from './sectionComponents/hero/hero8';
-import Hero9 from './sectionComponents/hero/hero9';
-import Hero10 from './sectionComponents/hero/hero10';
-import Hero11 from './sectionComponents/hero/hero11';
-import Hero12 from './sectionComponents/hero/hero12';
-import About1 from './sectionComponents/about/about1';
-import About2 from './sectionComponents/about/about2';
-import About3 from './sectionComponents/about/about3';
-import About4 from './sectionComponents/about/about4';
-import About5 from './sectionComponents/about/about5';
-import About6 from './sectionComponents/about/about6';
-import About7 from './sectionComponents/about/about7';
-import About8 from './sectionComponents/about/about8';
-import About9 from './sectionComponents/about/about9';
-import About10 from './sectionComponents/about/about10';
-import About11 from './sectionComponents/about/about11';
-import About12 from './sectionComponents/about/about12';
-import Course1 from './sectionComponents/course/course1';
-import More1 from './sectionComponents/morecourse/more1';
-import Test1 from './sectionComponents/testimonial/test1';
-import Course2 from './sectionComponents/course/course2';
-import Course3 from './sectionComponents/course/course3';
-import Course4 from './sectionComponents/course/course4';
-import Course5 from './sectionComponents/course/course5';
-import Course6 from './sectionComponents/course/course6';
-import Course7 from './sectionComponents/course/course7';
-import Course8 from './sectionComponents/course/course8';
-import Course9 from './sectionComponents/course/course9';
-import Course10 from './sectionComponents/course/course10';
-import Course11 from './sectionComponents/course/course11';
-import Course12 from './sectionComponents/course/course12';
-import More2 from './sectionComponents/morecourse/more2';
-import More3 from './sectionComponents/morecourse/more3';
-import More4 from './sectionComponents/morecourse/more4';
-import More5 from './sectionComponents/morecourse/more5';
-import More6 from './sectionComponents/morecourse/more6';
-import More7 from './sectionComponents/morecourse/more7';
-import More8 from './sectionComponents/morecourse/more8';
-import Test2 from './sectionComponents/testimonial/test2';
-import Test3 from './sectionComponents/testimonial/test3';
-import Test4 from './sectionComponents/testimonial/test4';
-import Test5 from './sectionComponents/testimonial/test5';
-import Test6 from './sectionComponents/testimonial/test6';
-import Test7 from './sectionComponents/testimonial/test7';
-import Test8 from './sectionComponents/testimonial/test8';
-import Test9 from './sectionComponents/testimonial/test9';
-import Test10 from './sectionComponents/testimonial/test10';
-import Test11 from './sectionComponents/testimonial/test11';
-import Table8 from './sectionComponents/table/table8';
-import Table9 from './sectionComponents/table/table9';
-import Table10 from './sectionComponents/table/table10';
-import Table11 from './sectionComponents/table/table11';
-import Table12 from './sectionComponents/table/table12';
+import { Navbar } from './components/navbar';
+import { Sidenavbar } from './components/sidenavbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <NavBar1/>
-      <NavBar2/>
-      <NavBar3/>
-      <Hero1/>
-      <Hero2/>
-      <Hero3/>
-      <Hero4/>
-      <Hero5/>
-      <Hero6/>
-      <Hero7/>
-      <Hero8/>
-      <Hero9/>
-      <Hero10/>
-      <Hero11/>
-      <Hero12/>
-      <About1/>
-      <About2/>
-      <About3/> 
-      <About4/>
-      <About5/>
-      <About6/>
-      <About7/>
-      <About8/>
-      <About9/>
-      <About10/>
-      <About11/>
-      <About12/>
-      <Course1/>
-      <Course2/>
-      <Course3/>
-      <Course4/>
-      <Course5/>
-      <Course6/>
-      <Course7/>
-      <Course8/>
-      <Course9/>
-      <Course10/>
-      <Course11/>
-      <Course12/>
-      <More1/>
-      <More2/>
-      <More3/>
-      <More4/>
-      <More5/>
-      <More6/>
-      <More6/>
-      <More7/>
-      <Table8/>
-      <Table9/>
-      <Table10/>
-      <Table11/>
-      <Table12/>
-      <Test1/>
-      <Test2/>
-      <Test3/>
-      <Test4/>
-      <Test5/>
-      <Test6/>
-      <Test7/>
-      <Test8/>
-      <Test9/>
-      <Test10/>
-      <Test11/>
+      <Navbar/>
+      <div className='flex'>
+     <Sidenavbar/>
+     {/* <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+   </Switch> */}
+     </div>
+
     </div>
   );
 }
