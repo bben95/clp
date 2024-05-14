8a68a042f96b6f92c510b53aeb34f33606d2d4df
bben95 <benakabp@gmail.com>
2024-05-14 12:48:17 +0530

    Changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { NavBar } from './components/navbar';
 import { Sidenavbar } from './components/sidenavbar';
 import { Route, Routes} from 'react-router-dom';
@@ -12,26 +12,39 @@ import { Table } from './pages/table';
 import { Testimonial } from './pages/testimonial';
 import FinalPage from './pages/filnalPage';
 function App() {
-  return (
-    <div className="App" >
-      <NavBar/>
-      <div className='flex'>
-     <Sidenavbar/>
-     <Routes>
-     <Route path="/" element={<Navbar/>} />
-     <Route   path="/hero" element={<Hero/>} />
-    <Route path="/about" element={<About/>} />
-    <Route path="/course" element={<Course/>} />
-    <Route path="/more" element={<More/>} />
-    <Route path="/table" element={<Table/>} />
-    <Route path="/testimonial" element={<Testimonial/>} />
-    <Route path="/final" element={<FinalPage/>} />
-     </Routes>
-     
-     </div>
+  const [isMobile, setIsMobile] = useState(false);
 
-    </div>
-  );
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Initial check
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return isMobile ? (
+    <p className="text-center">This app is disabled for mobile. Please open in desktop</p>
+  ) : (
+    <div className="App">
+      <NavBar />
+      <div className='flex'>
+        <Sidenavbar />
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/hero" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/more" element={<More />} />
+          <Route path="/table" element={<Table />} />
+          <Route path="/testimonial" element={<Testimonial />} />
+          <Route path="/final" element={<FinalPage />} />
+        </Routes>
+      </div>
+    </div>)
 }
 
 export default App;
